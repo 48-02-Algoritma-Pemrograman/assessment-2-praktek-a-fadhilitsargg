3d941ba90333c0a3855726032f56d4f8a0753881
fadhilitsargg <fadhilitsarjr@gmail.com>
2024-12-12 12:40:54 +0700

    Update Nomor2.java

--- Nomor2.java
+++ Nomor2.java
@@ -5,9 +5,23 @@ public class Nomor2 {
         Scanner input = new Scanner(System.in);
         int N = input.nextInt();
         /* Pekerjaan anda mulai dari sini */
+        
+        System.out.println("Deret angka dari 1 hingga " + N + ":");
+        for (int i = 1; i <= N; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        int sum = 0;
+        for (int i = 1; i <= N; i++) {
+            sum += i;
+        }
+        System.out.println("Jumlah deret angka dari 1 hingga " + N + " adalah: " + sum);
+
+        double rataRata = (double) sum / N;
+        System.out.println("Rata-rata deret angka dari 1 hingga " + N + " adalah: " + rataRata);
 
         /* Pekerjaan anda berakhir sini */
-
         System.out.println(Nomor2.class.getDeclaredMethods().length); 
     }
 }
